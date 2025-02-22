1a03b87d74612fed6a10c99cb561d0e0e11114c0
Omendra209 <omendra209@gmail.com>
2025-02-22 20:02:39 +0530

    [OMENDRA SINGH] Add. - 05 Submission of Regex Solutions

---
+++ RegexIPAddressValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class IPAddressValidator
+{
+    public static void Main(string[] args)
+    {
+        string ip = "192.168.1.1";
+        string invalidIP = "256.256.256.256";
+
+        Console.WriteLine(ValidateIPAddress(ip));           
+        Console.WriteLine(ValidateIPAddress(invalidIP));    
+    }
+
+    public static string ValidateIPAddress(string ip)
+    {
+        Regex ipRegex = new Regex(@"^([0-9]{1,3}\.){3}[0-9]{1,3}$");
+
+        if (ipRegex.IsMatch(ip) && ValidateOctets(ip))
+        {
+            return "Valid";
+        }
+        else
+        {
+            return "Invalid";
+        }
+    }
+
+    private static bool ValidateOctets(string ip)
+    {
+        string[] parts = ip.Split('.');
+        foreach (var part in parts)
+        {
+            if (int.Parse(part) < 0 || int.Parse(part) > 255)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ RegexSSNValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class SSNValidator
+{
+    public static void Main(string[] args)
+    {
+        string ssn1 = "123-45-6789";
+        string ssn2 = "123456789";
+
+        Console.WriteLine(ValidateSSN(ssn1));  // Valid
+        Console.WriteLine(ValidateSSN(ssn2));  // Invalid
+    }
+
+    public static string ValidateSSN(string ssn)
+    {
+        Regex ssnRegex = new Regex(@"^\d{3}-\d{2}-\d{4}$");
+
+        if (ssnRegex.IsMatch(ssn))
+        {
+            return ssn + " is valid";
+        }
+        else
+        {
+            return ssn + " is invalid";
+        }
+    }
+}
