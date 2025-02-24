bed75c97c99ce4e94516cd91f59f3a7dcd64c0d5
Omendra209 <omendra209@gmail.com>
2025-02-24 16:24:43 +0530

    [OMENDRA SINGH] Add. - 06 Submission of Annotations Solutions

---
+++ AnnotationObsoleteAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class LegacyAPI
+{
+    [Obsolete("OldFeature is deprecated, use NewFeature instead.")]
+    public void OldFeature()
+    {
+        Console.WriteLine("This is the old feature.");
+    }
+
+    public void NewFeature()
+    {
+        Console.WriteLine("This is the new feature.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LegacyAPI api = new LegacyAPI();
+        api.OldFeature(); // This will show a warning
+        api.NewFeature();
+    }
+}
